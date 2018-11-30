450ca7e187fc6f0da018b25f7439d23f977a8e0d
Simon Sundström <ssundstrom@live.se>
2018-11-30 11:32:06 +0100

    final version typechecker, bugfix

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -9,9 +9,10 @@ import CPP.Abs
 import CPP.Print
 import CPP.ErrM
 
-type Env = (Sig, [Context])
-type Sig = Map Id ([Type], Type)
+type Env = (DefMap, [Context])
+type DefMap = Map Id FunSig
 type Context = Map Id Type
+type FunSig = ([Type], Type)
 
 isOk :: Err a -> Bool
 isOk (Ok _) = True
@@ -22,7 +23,7 @@ isNumeric Type_int = True
 isNumeric Type_double = True
 isNumeric _ = False
 
-builtinFuns :: [(Id, ([Type], Type))]
+builtinFuns :: [(Id, FunSig)]
 builtinFuns = [
         (Id "printInt", ([Type_int], Type_void)),
         (Id "printDouble", ([Type_double], Type_void)),
@@ -33,12 +34,11 @@ builtinFuns = [
 typecheck :: Program -> Err ()
 typecheck (PDefs defs) = 
     if mainDef `notElem` userDefFuns
-    -- TODO: Print what is wrong with main instead of a generic error
     then fail "No main function with correct signature defined"
     else do
         env <- foldM updateFun' emptyEnv builtinFuns
         env' <- foldM updateFun' env userDefFuns
-        foldM_ checkDef env' defs
+        mapM_ (checkDef env') defs
     where
         mainDef = (Id "main", ([], Type_int))
         updateFun' env = uncurry (updateFun env)
@@ -67,8 +67,8 @@ checkStms returnType = foldM (checkStm returnType)
 
 checkStm :: Type -> Env -> Stm -> Err Env
 checkStm returnType env stm = case stm of
-    SExp x -> do
-        inferExp env x
+    SExp exp -> do
+        inferExp env exp
         return env
     SDecls varType (id:ids) -> 
         if varType == Type_void
@@ -77,38 +77,38 @@ checkStm returnType env stm = case stm of
             env' <- updateVar env id varType
             checkStm returnType env' $ SDecls varType ids
     SDecls _ [] -> return env
-    SInit varType id x ->
+    SInit varType id exp ->
         if varType == Type_void
         then fail $ "Error in " ++ printTree stm ++ ". Tried to create a variable of type void."
         else do
-            checkExp env x varType
+            checkExp env exp varType
             updateVar env id varType
-    SReturn x -> do
-        checkExp env x returnType
+    SReturn exp -> do
+        checkExp env exp returnType
         return env
-    SWhile x stm' -> do
-        checkExp env x Type_bool
-        checkStm returnType env stm'
+    SWhile exp stm' -> do
+        checkExp env exp Type_bool
+        checkStm returnType (newBlock env) stm'
         return env
-    SIfElse x stm1 stm2 -> do
-        checkExp env x Type_bool
-        checkStm returnType env stm1
-        checkStm returnType env stm2
+    SIfElse exp stm1 stm2 -> do
+        checkExp env exp Type_bool
+        checkStm returnType (newBlock env) stm1
+        checkStm returnType (newBlock env) stm2
         return env
     SBlock stms -> do
         checkStms returnType (newBlock env) stms
         return env
 
 checkExp :: Env -> Exp -> Type -> Err ()
-checkExp env x expectedType = do
-    inferredType <- inferExp env x
+checkExp env exp expectedType = do
+    inferredType <- inferExp env exp
     unless (expectedType == inferredType)
-        $ fail ("Type of " ++ printTree x
+        $ fail ("Type of " ++ printTree exp
             ++ ". Expected " ++ printTree expectedType
             ++ ", got " ++ printTree inferredType)
 
 inferExp :: Env -> Exp -> Err Type
-inferExp env x = case x of
+inferExp env exp = case exp of
     ETrue -> return Type_bool
     EFalse -> return Type_bool
     EInt _ -> return Type_int
@@ -116,33 +116,34 @@ inferExp env x = case x of
     EId id -> lookupVar env id
     EApp id args -> do
         (argTypes, retType) <- lookupFun env id
-        -- TODO: Don't swallow errors for checkExp for args
         if length argTypes == length args
-            && all isOk (zipWith (checkExp env) args argTypes)
-        then return retType
-        else fail $ "Type error in argument for function call " ++ printTree x
+            then do
+                mapM_ (uncurry $ checkExp env) $ zip args argTypes
+                return retType
+            else fail $ "Wrong number of arguments in function call " 
+                        ++ printTree exp
     EPostIncr id -> inferIncrDecr id
     EPostDecr id -> inferIncrDecr id
     EPreIncr id -> inferIncrDecr id
     EPreDecr id -> inferIncrDecr id
-    ETimes x1 x2 -> inferNumericOp x1 x2 "multiplication"
-    EDiv x1 x2 -> inferNumericOp x1 x2 "division"
-    EPlus x1 x2 -> inferNumericOp x1 x2 "addition"
-    EMinus x1 x2 -> inferNumericOp x1 x2 "subtraction"
-    ELt x1 x2 -> inferNumericOp x1 x2 "less than"
-    EGt x1 x2 -> inferNumericOp x1 x2 "greater than"
-    ELtEq x1 x2 -> inferNumericOp x1 x2 "less than or equal"
-    EGtEq x1 x2 -> inferNumericOp x1 x2 "greater than or equal"
-    EEq x1 x2 -> inferEq x1 x2 "equality"
-    ENEq x1 x2 -> inferEq x1 x2 "inequality"
-    EAnd x1 x2 -> inferJunc x1 x2 "conjunction"
-    EOr x1 x2 -> inferJunc x1 x2 "disjunction"
-    EAss id x -> do
+    ETimes exp1 exp2 -> inferNumericOp exp1 exp2 "multiplication"
+    EDiv exp1 exp2 -> inferNumericOp exp1 exp2 "division"
+    EPlus exp1 exp2 -> inferNumericOp exp1 exp2 "addition"
+    EMinus exp1 exp2 -> inferNumericOp exp1 exp2 "subtraction"
+    ELt exp1 exp2 -> inferComp exp1 exp2 "less than"
+    EGt exp1 exp2 -> inferComp exp1 exp2 "greater than"
+    ELtEq exp1 exp2 -> inferComp exp1 exp2 "less than or equal"
+    EGtEq exp1 exp2 -> inferComp exp1 exp2 "greater than or equal"
+    EEq exp1 exp2 -> inferEq exp1 exp2 "equality"
+    ENEq exp1 exp2 -> inferEq exp1 exp2 "inequality"
+    EAnd exp1 exp2 -> inferJunc exp1 exp2 "conjunction"
+    EOr exp1 exp2 -> inferJunc exp1 exp2 "disjunction"
+    EAss id exp -> do
         varType <- lookupVar env id
-        inferredType <- inferExp env x
+        inferredType <- inferExp env exp
         if varType == inferredType
         then return varType
-        else fail $ "Error in " ++ printTree x 
+        else fail $ "Error in " ++ printTree exp 
             ++ ". Tried to assign " ++ printTree inferredType ++ " to " 
             ++ printTree varType
     where
@@ -150,46 +151,51 @@ inferExp env x = case x of
             varType <- lookupVar env id'
             if isNumeric varType
             then return varType
-            else fail $ "Error in " ++ printTree x 
+            else fail $ "Error in " ++ printTree exp 
                 ++ ". Tried to increment/decrement " ++ printTree varType
         inferNumericOp = inferOp [Type_int, Type_double]
-        inferEq = inferOp [Type_int, Type_double, Type_bool]
+        inferComp = inferCompOp [Type_int, Type_double]
+        inferEq = inferCompOp [Type_int, Type_double, Type_bool]
         inferJunc = inferOp [Type_bool]
-        inferOp allowedTypes x1' x2' op = do 
-            inferredType <- inferExp env x1'
+        inferOp allowedTypes exp1' exp2' op = do 
+            inferredType <- inferExp env exp1'
             if inferredType `elem` allowedTypes
             then do
-                _ <- checkExp env x2' inferredType 
+                _ <- checkExp env exp2' inferredType 
                 return inferredType
-            else fail $  "Error in " ++ printTree x
+            else fail $  "Error in " ++ printTree exp
                 ++ ". Can't apply operation " ++ op 
                 ++ " to type " ++ printTree inferredType
+        inferCompOp allowedTypes exp1' exp2' op = do
+            _ <- inferOp allowedTypes exp1' exp2' op
+            return Type_bool
 
 lookupVar :: Env -> Id -> Err Type
-lookupVar (sig, con:contexts ) id = case Map.lookup id con of
+lookupVar (defMap, con:contexts ) id = case Map.lookup id con of
     Just varType -> Ok varType
-    Nothing -> lookupVar (sig, contexts) id
+    Nothing -> lookupVar (defMap, contexts) id
 lookupVar (_, []) id = Bad $ "No variable defined with id " ++ printTree id
 
-lookupFun :: Env -> Id -> Err ([Type], Type)
-lookupFun (sig, _) id = case Map.lookup id sig of
+lookupFun :: Env -> Id -> Err FunSig
+lookupFun (defMap, _) id = case Map.lookup id defMap of
     Just fSig -> Ok fSig
     Nothing -> Bad $ "No function defined with id " ++ printTree id
 
 updateVar :: Env -> Id -> Type -> Err Env
-updateVar (sig, con:contexts) id varType =
+updateVar env@(defMap, con:contexts) id varType =
     if Map.member id con
-    then fail $ "Variable already defined in context, " ++ printTree id
-    else return (sig, Map.insert id varType con : contexts)
+    then fail $ "Variable " ++ printTree id ++ " (" ++ printTree varType ++ ")"
+            ++ " already defined in context \n" ++ Map.showTreeWith (curry show) True True con
+    else return (defMap, Map.insert id varType con : contexts)
 
-updateFun :: Env -> Id -> ([Type], Type) -> Err Env
-updateFun (sig, contexts) id newSig =
-    if Map.member id sig
+updateFun :: Env -> Id -> FunSig -> Err Env
+updateFun (defMap, contexts) id newSig =
+    if Map.member id defMap
     then fail $ "Function name already defined, " ++ printTree id
-    else return (Map.insert id newSig sig, contexts)
+    else return (Map.insert id newSig defMap, contexts)
 
 newBlock :: Env -> Env
-newBlock (sig, contexts) = (sig, Map.empty:contexts)
+newBlock (defMap, contexts) = (defMap, Map.empty:contexts)
 
 emptyEnv :: Env
 emptyEnv = (Map.empty, [Map.empty])
